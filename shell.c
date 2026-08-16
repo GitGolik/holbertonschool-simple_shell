464fa92fcb0825cba64f03cade8bf9d8f821ea76
GitGolik <goliktheo.pro@gmail.com>
2026-08-16 15:22:04 +0200

    ADD func trim to SUPP spaces

--- shell.c
+++ shell.c
@@ -7,8 +7,43 @@
 extern char **environ;
 
 /**
- * main - entry point of shell
- * Return: 0 for success
+ * trim - remove space before and after the string
+ * @str: string to modifie
+ * Return: nothing
+ */
+void trim(char *str)
+{
+	char *start = str;
+	char *end;
+	size_t len;
+
+	while (*start == ' ' || *start == '\t')
+		start++;
+
+	len = strlen(start);
+	if (len == 0)
+	{
+		str[0] = '\0';
+		return;
+	}
+	end = start + len - 1;
+	while (end > start && (*end == ' ' || *end == '\t' || *end == '\n'))
+		end--;
+
+	if (start != str)
+	{
+		memmove(str, start, end - start + 1);
+		str[end - start + 1] = '\0';
+	}
+	else
+	{
+		*(end + 1) = '\0';
+	}
+}
+
+/**
+ * main - entru point of shell
+ * Return: 0 success
  */
 int main(void)
 {
@@ -25,8 +60,8 @@ int main(void)
 	{
 		if (interactive)
 		{
-		printf("#cisfun$ ");
-		fflush(stdout);
+			printf("#cisfun$ ");
+			fflush(stdout);
 		}
 
 		nread = getline(&command, &len, stdin);
@@ -35,17 +70,20 @@ int main(void)
 		{
 			if (interactive)
 				printf("\n");
-
 			free(command);
 			exit(0);
 		}
 
 		if ((nread > 0) && (command[nread - 1] == '\n'))
 			command[nread - 1] = '\0';
+
+		trim(command);
+
 		if (command[0] == '\0')
 			continue;
 
 		pid = fork();
+
 		if (pid == -1)
 		{
 			perror("./hsh");
@@ -55,15 +93,19 @@ int main(void)
 		if (pid == 0)
 		{
 			char *args[2];
+
 			args[0] = command;
 			args[1] = NULL;
 
 			execve(command, args, environ);
-			perror(command);
+
+			perror("./hsh");
 			exit(127);
 		}
 		else
+		{
 			waitpid(pid, &status, 0);
+		}
 	}
 
 	free(command);
