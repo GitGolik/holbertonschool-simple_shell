18bc6f1894f6bfc0603e56d6a5af7c122dbc7817
GitGolik <goliktheo.pro@gmail.com>
2026-08-19 13:35:39 +0200

    ADD get_path_env.c 1.0 to get the path

---
+++ get_path_env.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * get_path_env - get the value of PATH from the environment
+ * Return: value of PATH, or NULL if not found
+ */
+char *get_path_env(void)
+{
+	int i;
+	char **env;
+
+	env = environ;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		if (strncmp(env[i], "PATH=", 5) == 0)
+			return (env[i] + 5);
+	}
+
+	return (NULL);
+}
